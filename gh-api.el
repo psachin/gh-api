fa741265495b96e3135a7863c3c418a8df9c0a25
psachin <iclcoolster@gmail.com>
2014-03-19 20:35:34 +0530

    Subject: Added license and file header.
    Description:

--- gh-api.el
+++ gh-api.el
@@ -1,8 +1,26 @@
+;;; gh-api.el --- GitHub client for Emacs
 
+;; Copyright (C) 2014 by Sachin Patil
+
+;; Author: Sachin Patil <isachin@iitb.ac.in>
+;; URL: http://github.com/psachin/gh-api
+;; Keywords: github, api, tool, convenience
+;; Version: 0.9
+
+;; This file is NOT a part of GNU Emacs.
+
+;; `gh-api' is free software distributed under the terms of the GNU
+;; General Public License, version 3 or later. For details, see the
+;; file COPYING.
+
+;;; Commentary:
+;; GitHub client for Emacs
+;; URL: http://github.com/psachin/gh-api
+
+;;; Code:
 
 (require 'oauth2)
 (require 'json)
-
 
 (defvar my-token
   (make-oauth2-token :access-token ""))
@@ -12,9 +30,6 @@
       (oauth2-url-retrieve-synchronously my-token "https://api.github.com/user")
     (goto-char url-http-end-of-headers)
     (json-read)))
-
-
-
 
 (replace-regexp-in-string "{/gist_id}" "" (cdr (assoc 'gists_url user-data)))
 
